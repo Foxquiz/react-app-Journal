7da06fcecee12134c6539265140992c7983df1bf
Arina Bardyukova <arina.bardyukova@yandex.ru>
2024-02-22 19:57:37 +0300

    adds useRef

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,22 +1,36 @@
 import styles from './JournalForm.module.css';
 import Button from '../Button/Button';
 import cn from 'classnames';
-import { useEffect, useState } from 'react';
-
-const INITIAL_STATE = {
-	title: true,
-	post: true,
-	date: true
-};
+import { useEffect, useReducer, useRef } from 'react';
+import { INITIAL_STATE, formReducer } from './JournalForm.state';
 
 function JournalForm({ onSubmit }) {
-	const [formValidateState, setFormValidateState] = useState(INITIAL_STATE);
+	const [formState, dispatchForm] = useReducer(formReducer, INITIAL_STATE);
+	const { isValid, isFormReadyToSubmit, values } = formState; //деструктуризация, подписка на isValid
+	const titleRef = useRef();
+	const dateRef = useRef();
+	const postRef = useRef();
+
+	const focusError = (isValid) => {
+		switch (true) {
+		case !isValid.title:
+			titleRef.current.focus();
+			break;
+		case !isValid.date:
+			dateRef.current.focus();
+			break;
+		case !isValid.post:
+			postRef.current.focus();
+			break;
+		}
+	};
 
 	useEffect(() => {
 		let timerId;
-		if (!formValidateState.date || !formValidateState.title || !formValidateState.post) {
+		if (!isValid.date || !isValid.title || !isValid.post) {
 			timerId = setTimeout(() => {
-				setFormValidateState(INITIAL_STATE);
+				focusError(isValid);
+				dispatchForm({ type: 'RESET_VALIDITY' });
 			}, 2000);
 		}
 
@@ -24,54 +38,43 @@ function JournalForm({ onSubmit }) {
 		//когда компонент исчезает, то выполняется функция ИЛИ когда снова меняется и вызывается эффект/hook
 		//убирает мерцание/повторный вызов
 
-	}, [formValidateState]);
+	}, [isValid]);
+
+	useEffect(() => {
+		if (isFormReadyToSubmit) {
+			onSubmit(values);
+			dispatchForm({ type: 'CLEAN' });
+		}
+	}, [isFormReadyToSubmit, values, onSubmit]);
 
 	const addJournalItem = (e) => {
 		e.preventDefault();
-		const formData = new FormData(e.target);
-		const formProps = Object.fromEntries(formData);
-		let isFormValid = true;
-		if (!formProps.title?.trim().length) {
-			setFormValidateState(state => ({ ...state, title: false }));
-			isFormValid = false;
-		} else {
-			setFormValidateState(state => ({ ...state, title: true }));
-		}
-		if (!formProps.post?.trim().length) {
-			setFormValidateState(state => ({ ...state, post: false }));
-			isFormValid = false;
-		} else {
-			setFormValidateState(state => ({ ...state, post: true }));
-		}
-		if (!formProps.date) {
-			setFormValidateState(state => ({ ...state, date: false }));
-			isFormValid = false;
-		} else {
-			setFormValidateState(state => ({ ...state, date: true }));
-		}
-		if (!isFormValid) return;
-		onSubmit(formProps);
+		dispatchForm({ type: 'SUBMIT' });
+	};
+
+	const onChange = (e) => {
+		dispatchForm({ type: 'SET_VALUE', payload: { [e.target.name]: e.target.value } });
 	};
 
 	return (
 		<form className={styles['journal-form']} onSubmit={addJournalItem}>
-			<input type="text" name='title' className={cn(styles['input'], styles['journal-form__title'], {
-				[styles['invalid']]: !formValidateState.title
+			<input type="text" ref={titleRef} onChange={onChange} value={values.title} name='title' className={cn(styles['input'], styles['journal-form__title'], {
+				[styles['invalid']]: !isValid.title
 			})} />
 			<label className={cn(styles['journal-form__date-row'])}>
 				<img className={styles['journal-form__img']} src="/calendar.svg" alt="calendar" />
 				<span className={styles['journal-form__text']}>Дата</span>
-				<input type="date" name='date' className={cn(styles['input'], {
-					[styles['invalid']]: !formValidateState.date
+				<input type="date" ref={dateRef} onChange={onChange} value={values.data} name='date' className={cn(styles['input'], {
+					[styles['invalid']]: !isValid.date
 				})} />
 			</label>
 			<label className={cn(styles['journal-form__date-row'])}>
 				<img className={styles['journal-form__img']} src="/folder.svg" alt="folder" />
 				<span className={styles['journal-form__text']}>Метки</span>
-				<input type="text" name='tag' className={cn(styles['input'])} />
+				<input type="text" onChange={onChange} value={values.tag} name='tag' className={cn(styles['input'])} />
 			</label>
-			<textarea name="post" id="" cols="30" rows="10" className={cn(styles['input'], styles['journal-form__textarea'], {
-				[styles['invalid']]: !formValidateState.post
+			<textarea name="post" ref={postRef} onChange={onChange} value={values.post} id="" cols="30" rows="10" className={cn(styles['input'], styles['journal-form__textarea'], {
+				[styles['invalid']]: !isValid.post
 			})}></textarea>
 			<Button text='Save' />
 		</form >
